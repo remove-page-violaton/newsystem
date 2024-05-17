0b5e901cace16e21239fba102604f378be9b5a03
remove-page-violaton <167241675+remove-page-violaton@users.noreply.github.com>
2024-05-16 22:07:43 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <title>Loading</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Responsive meta tag added -->
+    <!-- Other meta tags -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Other styles and scripts -->
+</head>
+<body>
+    <!-- Your existing HTML content -->
+    
+    <!-- Include script.js which contains the loading functionality -->
+    <script src="script.js"></script>
+</body>
+</html>
